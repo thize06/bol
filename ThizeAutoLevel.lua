7047c9764cfc3fd1047cb6f99b4713623a03a171
thizebol <giovanimotalobato@hotmail.com>
2015-07-25 01:01:20 -0300

    Update ThizeAutoLevel.lua

--- ThizeAutoLevel.lua
+++ ThizeAutoLevel.lua
@@ -25,7 +25,7 @@ local _autoLevel = { spellsSlots = { SPELL_1, SPELL_2, SPELL_3, SPELL_4 }, level
 local __autoLevel__OnTick
 local rOFF=0
 --update func--
-local version = "4.00"
+local version = "2.14"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/thize06/Bol/ThizeAutoLevel.lua".."?rand="..math.random(1,10000)
@@ -34,16 +34,16 @@ local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function _AutoupdaterMsg(msg) print("<b><font color=\"#FF0000\">Thize Auto Level:</font></b> <font color=\"#FFFFFF\">"..msg.."</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/prado1506/Bol1/master/AutoLevelSkillTyler1.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/thize06/bol/master/Thize%20Auto%20Level%20Spells.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
 			if tonumber(version) < ServerVersion then
-				_AutoupdaterMsg("New version available "..ServerVersion)
-				_AutoupdaterMsg("Updating, please don't press F9")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+				_AutoupdaterMsg("Nova versao Disponivel "..ServerVersion)
+				_AutoupdaterMsg("Atualizando, porfavor nao aperte F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Sucesso ao Atualizar ("..version.." => "..ServerVersion.."), aperte F9 para carregar a versao mais nova") end) end, 3)
 			else
-				_AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+				_AutoupdaterMsg("Voce tem a versao mais recente ("..ServerVersion..")")
 			end
 		end
 	else
@@ -85,8 +85,8 @@ function autoLevelSetSequenceCustom(sequence)
 end
 
 function OnLoad()
-	print("<b><font color=\"#FF0000\">Thize Auto Level:</font></b> <font color=\"#FFFFFF\">Loading...</font>"..version)
+	print("<b><font color=\"#FF0000\">Thize Auto Level:</font></b> <font color=\"#FFFFFF\">Carregando...</font>"..version)
 	Menu = scriptConfig("["..myHero.charName.." - AutoLevel]", player.charName.."AutoLevel")
 	Menu:addParam("sequenceSpells", "Sequence Spells", SCRIPT_PARAM_LIST, 1, { 'R-Q-W-E',})
-	print("<b><font color=\"#FF0000\">Thize Auto Level:</font></b> <font color=\"#FFFFFF\">Sucessfully Loaded..!</font>")
+	print("<b><font color=\"#FF0000\">Thize Auto Level:</font></b> <font color=\"#FFFFFF\">Sucesso ao Carregar - Seja Bem Vindo..!</font>")
 end
